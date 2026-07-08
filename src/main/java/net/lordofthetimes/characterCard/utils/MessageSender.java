85933835a23ea4ee2f6ce39ae2499069e76c43d7
LordofTheTime <a@a.a>
2026-07-09 00:11:09 +0200

    add "seconds"to cooldown message

--- src/main/java/net/lordofthetimes/characterCard/utils/MessageSender.java
+++ src/main/java/net/lordofthetimes/characterCard/utils/MessageSender.java
@@ -21,7 +21,7 @@ public class MessageSender {
     }
 
     public static void sendCooldown(CommandSender sender, long time){
-        sendMessage(sender,"<red>This command is on cooldown! Please try again in <bold>" + time/1000f +"</bold> !</red>");
+        sendMessage(sender,"<red>This command is on cooldown! Please try again in <bold>" + time/1000f +"</bold> seconds!</red>");
     }
 
     public static Component deserialize(String input) {
